2e3a395562a933bb79bb94c76b14d36be3cdb438
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-01-21 16:20:52 -0800

    video demo working

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+const port = 8080;
+
+const users = {};
+
+// socket.io listeners go here
+io.on('connection', socket => {
+    if (!users[socket.id]) {
+      users[socket.id] = socket.id;
+      console.log(`User ${socket.id} connected`);
+      
+    }
+    socket.emit("yourID", socket.id);
+    io.sockets.emit("allUsers", users);
+    socket.on('disconnect', () => {
+      delete users[socket.id];
+    })
+
+    socket.on("callUser", (data) => {
+      io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
+    })
+
+    socket.on("acceptCall", (data) => {
+      io.to(data.to).emit('callAccepted', data.signal);
+    })
+});
+
+server.listen(port, () => {
+    console.log(`socket.io server listening on ${port}`)
+});
