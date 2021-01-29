234bfffd547990bde511d406b2bbf3bcd987d222
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-01-28 16:48:49 -0800

    create sidebar

--- client/src/views/ChatRoom/ChatRoom.js
+++ client/src/views/ChatRoom/ChatRoom.js
@@ -1,9 +1,64 @@
 import React from 'react';
 import Caller from 'components/Caller';
+import { 
+  Checkbox,
+  Grid, 
+  Image,
+  Header,
+  Icon,
+  Menu,
+  Segment,
+  Sidebar
+} from 'semantic-ui-react';
 
 function ChatRoom() {
+  const [visible, setVisible] = React.useState(false);
+
   return (
-    <h2>Chat Room</h2>
+    <Grid columns={1}>
+      <Grid.Column>
+        <Checkbox
+          checked={visible}
+          label={{ children: <code>visible</code> }}
+          onChange={(e, data) => setVisible(data.checked)}
+        />
+      </Grid.Column>
+
+      <Grid.Column>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            direction='right'
+            icon='labeled'
+            onHide={() => setVisible(false)}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Grid.Column>
+    </Grid>
   );
 }
 
