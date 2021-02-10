d86c2043f9ff04590c6881e31a2bf05daaaf3654
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-02-10 15:17:29 -0800

    add route to add and remove patients from waiting room

--- src/routes/waiting-room.js
+++ src/routes/waiting-room.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const R = require('ramda');
+
+let waitingRoom = {
+  count: 2,
+  patients: [ '1', '2' ]
+}
 
 router.get('/', (_, res) => {
-  console.log('/waitingroom queryied');
-  res.send('response from server');
+  console.log('queried waiting room.');
+  res.json(waitingRoom);
 });
+
+router.route('/patient/:id')
+  .post((req, res) => {
+    waitingRoom.count = waitingRoom.count + 1;
+    waitingRoom.patients.push(req.params.id);
+    res.json(waitingRoom);;
+  })
+  .delete((req, res) => {
+    const neqId = id => id !== req.params.id;
+    const transform = {
+      count: R.add(-1),
+      patients: R.filter(neqId)
+    };
+    const newWaitingRoom = R.evolve(transform, waitingRoom);
+    waitingRoom = newWaitingRoom
+
+    console.log(`removed patient Id: ${req.params.id}`);
+    res.json(waitingRoom);
+  });
 
 module.exports = router;
