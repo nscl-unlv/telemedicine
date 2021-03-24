1f4ba17c3a763feac48f0bb0b079ebc8c943f9b3
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-03-24 15:55:48 -0700

    add checkin

--- client/src/views/CheckIn/CheckIn.js
+++ client/src/views/CheckIn/CheckIn.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Button, Container, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import checkmark from './images/checkmark.png';
+import './CheckIn.css';
 
 function CheckIn() {
   return (
     <>
-      <h1>Check-In Menu</h1>
+      <Container text textAlign="center">
+        <Image id="checkmark-img" src={checkmark} size="small" fluid centered />
+        <h3>
+          Please click the button to check in. You will be re-routed to the
+          waiting room.
+        </h3>
+        <Link to="/waitingroom">
+          <Button primary>Check In</Button>
+        </Link>
+      </Container>
     </>
   );
 }
