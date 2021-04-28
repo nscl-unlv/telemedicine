fa4c54f50ee30856ca02821432631443e29c19f6
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-04-28 15:22:04 -0700

    turn of console log eslint

--- client/src/views/ChatRoom/ChatRoom.js
+++ client/src/views/ChatRoom/ChatRoom.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import Caller from 'components/Caller';
 import ChatRoomMenu from 'components/ChatRoomMenu';
 import {
-  Button, Icon, Image, Menu, Sidebar,
+  Button, Icon, Menu, Sidebar,
 } from 'semantic-ui-react';
 import Receiver from 'components/Receiver';
 import { SocketContext } from 'contexts/SocketContext';
